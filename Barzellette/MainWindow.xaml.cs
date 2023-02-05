3c55ebc961e1b0c3309dda5731f52a6b329e28c1
Giulio Sorrentino <gsorre84@gmail.com>
2023-02-05 06:34:04 +0100

    aggiunti i comandi avanti, indietro e random

--- Barzellette/MainWindow.xaml.cs
+++ Barzellette/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace Barzellette
     public partial class MainWindow : Window
     {
         private OleDbConnection dbConnection;
-
+        private Random r;
         public MainWindow()
         {
             InitializeComponent();
@@ -44,6 +44,7 @@ namespace Barzellette
                 Application.Current.Shutdown();
 
             }
+            r=new Random();
             Application.Current.MainWindow.Closing += new CancelEventHandler(MainWindow_Closing);
         }
 
@@ -55,6 +56,7 @@ namespace Barzellette
         private void btn_Clicked(object sender, RoutedEventArgs e)
         {
             ulong id = 0;
+            wbbarzelletta.NavigateToString("&nbsp;");
             try
             {
                  id = ulong.Parse(txtid.Text);
@@ -74,6 +76,9 @@ namespace Barzellette
             {
                 wbbarzelletta.NavigateToString(ex.Message);
                 btnVisualizza.IsEnabled = false;
+                btnAvanti.IsEnabled = false;
+                btnIndietro.IsEnabled = false;
+                btnRandom.IsEnabled = false;
                 return;
             }
             if (myReader.HasRows)
@@ -88,6 +93,70 @@ namespace Barzellette
                 s = s.Replace("ù", "&ugrave;"); ;
                 wbbarzelletta.NavigateToString(s);
             } else
+            {
+                wbbarzelletta.NavigateToString("L'id selezionato non &egrave; stato trovato");
+            }
+        }
+
+        private void btnIndietro_Clicked(object sender, RoutedEventArgs e)
+        {
+            ulong id;
+            wbbarzelletta.NavigateToString("&nbsp;");
+            try
+            {
+                id = ulong.Parse(txtid.Text);
+            }
+            catch (System.FormatException)
+            {
+                wbbarzelletta.NavigateToString("Il campo id non &egrave; intero.");
+                return;
+            }
+            txtid.Text = $"{--id}";
+        }
+
+        private void btnAvanti_Clicked(object sender, RoutedEventArgs e)
+        {
+            ulong id;
+            wbbarzelletta.NavigateToString("&nbsp;");
+            try
+            {
+                id = ulong.Parse(txtid.Text);
+            }
+            catch (System.FormatException)
+            {
+                wbbarzelletta.NavigateToString("Il campo id non &egrave; intero.");
+                return;
+            }
+            txtid.Text = $"{++id}";
+
+        }
+
+        private void btnRandom_Clicked(object sender, RoutedEventArgs e)
+        {
+            DataSet myDataSet = new DataSet();
+            var myAdapptor = new OleDbDataAdapter();
+            OleDbCommand command = new OleDbCommand($"Select top 1 ID as MaxId FROM Barzellette order by ID desc ", dbConnection);
+            OleDbDataReader myReader;
+            try
+            {
+                myReader = command.ExecuteReader();
+            }
+            catch (System.Data.OleDb.OleDbException ex)
+            {
+                wbbarzelletta.NavigateToString(ex.Message);
+                btnVisualizza.IsEnabled = false;
+                btnAvanti.IsEnabled = false;
+                btnIndietro.IsEnabled = false;
+                btnRandom.IsEnabled = false;
+                return;
+            }
+            if (myReader.HasRows)
+            {
+                myReader.Read();
+                int id = myReader.GetInt32(0);
+                txtid.Text = $"{r.Next(id)}";
+            }
+            else
             {
                 wbbarzelletta.NavigateToString("L'id selezionato non &egrave; stato trovato");
             }
